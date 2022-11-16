65df70e5023e1e0c5bffef0e721081d3c2b1fcb9
lynconlassuncao <lynconlfelipe12@gmail.com>
2022-11-16 15:37:55 -0300

    Ajustes necessários para rodar

--- GerenciamentoProduto/FormBemVindo.cs
+++ GerenciamentoProduto/FormBemVindo.cs
@@ -13,48 +13,15 @@ namespace GerenciamentoProduto
 {
     public partial class FormBemVindo : Form
     {
-
-        static String caminhoDB = @"C:\Users\2018101836\Downloads\GerenciamentoProduto\BancoDeDados\banco.txt";
-
         public FormBemVindo()
         {
             InitializeComponent();
         }
 
-        private List<Produto> carregarProdutosDB()
-        {
-            List<Produto> produtos = new List<Produto>();
-            var linhas = File.ReadAllLines(caminhoDB).ToList();
-
-            //a fazer, ver como remover o primeiro elemento de uma lista
-            linhas.ForEach(linha => {
-                var campos = linha.Split(';');
-                var codigo = campos[0];
-                var nome = campos[1];
-                var precoCompra = Double.Parse(campos[2]);
-                var precoVenda = Double.Parse(campos[3]);
-                var peso = Double.Parse(campos[4]);
-                var quantidade = int.Parse(campos[5]);
-
-                var produto = new Produto(
-                   codigo,
-                   nome,
-                   peso,
-                   precoCompra,
-                   precoVenda,
-                   quantidade);
-
-                produtos.Add(produto);
-            });
-
-            return produtos;
-        }
-
         private void novoProdutoToolStripMenuItem_Click(object sender, EventArgs e)
         {
             var telaCadastro = new FormCadastroProduto();
-
-            var produtos = carregarProdutosDB();
+            var produtos = Produto.buscarProdutos();
 
             telaCadastro.ShowDialog();
         }
